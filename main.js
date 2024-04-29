f2edd0ee8498b931dbe6c927e49ef45d09e814f9
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-04-29 16:37:45 +0200

    ADDED BUTTON

--- main.js
+++ main.js
@@ -2,40 +2,78 @@
 
 
 
-const kmUser = Number(document.getElementById('kmUser'));//RECUPERA I KM DALL'UTENTE
+let kmUser = document.getElementById('kmUser');//RECUPERA I KM DALL'UTENTE
+kmUser = +(kmUser.value);//RECUPERA IL VALORE DEL CAMPO DEI CHILOMETRI E LO CAMBIA DA STRING À NUMBER
 
-const yearUser = Number(document.getElementById('userAge'));
+let yearUser = document.getElementById('userAge');//RECUPERA L'ETA DALL'UTENTE
+yearUser = +(yearUser.value);//RECUPERA IL VALORE DEL CAMPO DELL'ETA  E LO CAMBIA DA STRING À NUMBER
 
-const  kmPrice = 0.21;
+const kmPrice = 0.21;//PREZZO AL KM
 
-const priceKmUser = kmPrice * kmUser ;
-let price = priceKmUser;
-const underAge = 18;
-const over65 = 65;
+let priceKmUser = kmPrice * kmUser;//CALCOLO DEL PREZZO PER I CHILOMETRI PERCORSI DALL'UTENTE
 
-const saleUnderAge = 20;
-const saleOver65 = 40;
 
-if (underAge>yearUser) {
-   price= price - (price/100*saleUnderAge)
+
+
+
+
+
+
+
+
+
+
+
+
+const button = document.getElementById("ciao");
+
+button.addEventListener('click', function() {
+    console.log("ho cliccato il bottone")
+    console.log (yearUser)
+    console.log (kmUser)
+    console.log (kmPrice)
+    console.log (priceKmUser)
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const  kmPrice = 0.21;
+
+// const priceKmUser = kmPrice * kmUser ;
+// let price = priceKmUser;
+// const underAge = 18;
+// const over65 = 65;
+
+// const saleUnderAge = 20;
+// const saleOver65 = 40;
+
+// if (underAge>yearUser) {
+//    price= price - (price/100*saleUnderAge)
    
-}else if(over65<yearUser) {
-    price=price- (price/100*saleOver65)
-}
+// }else if(over65<yearUser) {
+//     price=price- (price/100*saleOver65)
+// }
 
 
-price = price.toFixed(2);
+// price = price.toFixed(2);
 
 
-let  currentText;
-currentText = `
-km da percorrere: ${kmUser}  
-anni dell'utente: ${yearUser} 
-prezzo al km ${kmPrice}€     
-prezzo dei km da percorrere ${priceKmUser}€
-prezzo finale ${price}€
-`;
-
-
-
-console.log(currentText);
