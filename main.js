feab1f74f455abb19a3df7a19257e2d8415d9d26
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-04-29 17:25:14 +0200

    MILESTONE1

--- main.js
+++ main.js
@@ -2,37 +2,86 @@
 
 
 
-let kmUser = document.getElementById('kmUser');//RECUPERA I KM DALL'UTENTE
-kmUser = +(kmUser.value);//RECUPERA IL VALORE DEL CAMPO DEI CHILOMETRI E LO CAMBIA DA STRING À NUMBER
 
-let yearUser = document.getElementById('userAge');//RECUPERA L'ETA DALL'UTENTE
-yearUser = +(yearUser.value);//RECUPERA IL VALORE DEL CAMPO DELL'ETA  E LO CAMBIA DA STRING À NUMBER
 
-const kmPrice = 0.21;//PREZZO AL KM
-
-let priceKmUser = kmPrice * kmUser;//CALCOLO DEL PREZZO PER I CHILOMETRI PERCORSI DALL'UTENTE
+const btnGenerate = document.getElementById ("generate");
+const btnReset = document.getElementById ("reset");
 
 
 
 
+btnGenerate.addEventListener('click', function(){
+    let userName = document.getElementById('name');//RECUPERA IL NOME DALL'UTENTE
+    userName = (userName.value);//RECUPERA IL VALORE DEL CAMPO DEL NOME 
+
+    let kmUser = document.getElementById('kmUser');//RECUPERA I KM DALL'UTENTE
+    kmUser = +(kmUser.value);//RECUPERA IL VALORE DEL CAMPO DEI CHILOMETRI E LO CAMBIA DA STRING À NUMBER
+
+    let yearUser = document.getElementById('userAge');//RECUPERA L'ETA DALL'UTENTE
+    yearUser = +(yearUser.value);//RECUPERA IL VALORE DEL CAMPO DELL'ETA  E LO CAMBIA DA STRING À NUMBER
+
+    const kmPrice = 0.21;//PREZZO AL KM
+
+    let priceKmUser = kmPrice * kmUser;//CALCOLO DEL PREZZO PER I CHILOMETRI PERCORSI DALL'UTENTE
+    
+    const underAge = 18;// variabile eta minorenni
+    const overAge = 65;//variabileeta over 65
+
+    const discountUnder = 20; //variabile sconto minorenni
+    const discountOver = 40; //variabile sconto over65
+    let userDiscount = 0;
+
+    if (underAge>yearUser) {
+        priceKmUser= priceKmUser - (priceKmUser/100*discountUnder);
+        userDiscount = discountUnder;
+    
+    }else if(overAge<yearUser) {
+        priceKmUser=priceKmUser- (priceKmUser/100*discountOver);
+        userDiscount = discountOver;
+    }
 
 
+    priceKmUser = priceKmUser.toFixed(2);
+    const price= priceKmUser;
 
+    let tiketPrice = document.getElementById('tiketprice');
+    tiketPrice.append(`  ${price}`);
 
+    let tiketName = document.getElementById('tiketname');
+    tiketName.append(`  ${userName}`);
 
+    const tiketDistance = document.getElementById('tiketdistance');
+    tiketDistance.append(`  ${kmUser}`)
 
+    const tiketDiscount = document.getElementById('tiketdiscount');
+    tiketDiscount.append(`  ${userDiscount}`)
 
+    const tiketAge = document.getElementById('tiketage');
+    tiketAge.append(`  ${yearUser}`)
 
-
-
-const button = document.getElementById("ciao");
-
-button.addEventListener('click', function() {
-    console.log("ho cliccato il bottone")
+        
+    
     console.log (yearUser)
     console.log (kmUser)
     console.log (kmPrice)
     console.log (priceKmUser)
+    console.log (tiketPrice)
+    console.log (userName)
+    
+ 
+})
+
+
+
+
+
+
+
+
+
+reset.addEventListener('click', function() {
+    console.log("ho cliccato il bottone")
+    
 
 
 })
